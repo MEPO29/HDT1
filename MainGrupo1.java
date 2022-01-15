67c32bed82ac68b2f1ecd5149d6d78b4ed851899
montsegh4545 <80225989+montsegh4545@users.noreply.github.com>
2022-01-14 22:18:48 -0600

    Create MainGrupo1.java

---
+++ MainGrupo1.java
@@ -0,0 +1,14 @@
+/******************************************************************
+Ultima modificacion: 14/01/2022
+
+Clase main grupo 1
+@file MainGrupo1.java
+
+******************************************************************/
+
+
+public class MainGrupo1{
+    public static void main(String[] args){
+        
+    }
+}
