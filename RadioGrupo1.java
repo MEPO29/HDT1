26bc64e0fe7b58006414f271ecd0780054d25d64
Manuel <manuelalejandromartinezf@gmail.com>
2022-01-17 09:07:40 -0600

    Implementar RadioGrupo1

--- RadioGrupo1.java
+++ RadioGrupo1.java
@@ -10,11 +10,11 @@ public class RadioGrupo1 implements Radio{
     private boolean frecuencia;
     private double estacion;
     private boolean estado;
-    private int guardados[] = new int[12];
+    private double[] guardados = new double[12];
 
     
 
-    public RadioGrupo1(boolean frecuencia, double estacion, boolean estado, int[] guardados) {
+    public RadioGrupo1(boolean frecuencia, double estacion, boolean estado, double[] guardados) {
         this.frecuencia = frecuencia;
         this.estacion = estacion;
         this.estado = estado;
@@ -22,17 +22,19 @@ public class RadioGrupo1 implements Radio{
     }
 
     public RadioGrupo1(){
-        
+        this.frecuencia = true;
+        this.estacion = 530;
+        this.estado = true;
     }
 
 
     public boolean isFrecuencia() {
         return frecuencia;
     }
-    public int[] getGuardados() {
+    public double[] getGuardados() {
         return guardados;
     }
-    public void setGuardados(int guardados[]) {
+    public void setGuardados(double guardados[]) {
         this.guardados = guardados;
     }
     public boolean isEstado() {
@@ -50,6 +52,90 @@ public class RadioGrupo1 implements Radio{
     public void setFrecuencia(boolean frecuencia) {
         this.frecuencia = frecuencia;
     }
+
+	@Override
+	public boolean isOn() {
+		// TODO Auto-generated method stub
+		return estado;
+	}
+
+	@Override
+	public void turnOnOff() {
+		// TODO Auto-generated method stub
+		estado = !estado;
+		
+	}
+
+	@Override
+	public void nextStation(boolean station) {
+		// TODO Auto-generated method stub
+		if (frecuencia) {
+			estacion += 10;
+			if (estacion > 1610) {
+				estacion = 530;
+			}
+		} else {
+			estacion += 0.2;
+			if (estacion > 107.9) {
+				estacion = 87.9;
+			}
+		}
+	}
+
+	@Override
+	public void prevStation(boolean station) {
+		// TODO Auto-generated method stub
+		if (frecuencia) {
+			estacion -= 10;
+			if (estacion < 530) {
+				estacion = 1610;
+			}
+		} else {
+			estacion -= 0.2;
+			if (estacion < 87.9) {
+				estacion = 107.9;
+			}
+		}
+		
+	}
+
+	@Override
+	public double getStation() {
+		// TODO Auto-generated method stub
+		return estacion;
+	}
+
+	@Override
+	public void saveStation(int position, double station) {
+		// TODO Auto-generated method stub
+		this.guardados[position] = station;
+		
+	}
+
+	@Override
+	public double getSavedStation(int position) {
+		// TODO Auto-generated method stub
+		estacion = this.guardados[position];
+		frecuencia = estacion > 200;
+		return estacion;
+	}
+
+	@Override
+	public boolean getFrequency() {
+		// TODO Auto-generated method stub
+		return frecuencia;
+	}
+
+	@Override
+	public void switchAMFM() {
+		// TODO Auto-generated method stub
+		frecuencia = !frecuencia;
+		if (frecuencia) {
+			estacion = 530;
+		} else {
+			estacion = 87.9;
+		}
+	}
 
     
 }
